5f02119bb42611ed913b30940c2ad35e92c7e987
Iuri Severo <nobreiuri@gmail.com>
2019-03-19 20:27:09 -0300

    Alteração dos nomes das variáveis Função remove e show

--- graphs.cpp
+++ graphs.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include "graphs.hpp"
-Graph::Graph(){
+Subject::Subject(){ 
 
 }
-Graph::Graph(char matter_name[20], int code, int credits){
-    this->name = matter_name;
+Subject::Subject(string subject_name, int code, int credits){
+    this->name = subject_name;
     this->code = code;
     this->credits = credits;
 }
-Graph::~Graph(){
+Subject::~Subject(){
     //tenho que passar por todas os galhos para verificar a presença da matéria removida
 }
-void Graph::Insert_Edge(Graph *matter){
-    edges.push_back(*matter);
+void Subject::insert_prerequisite(Subject *subject){
+    if(subject)
+    	prerequisites.insert(*(subject));
 }
-void Graph::Show(){
-    cout<<edges.front().name<<endl;
+
+void Subject::remove_prerequisite(Subject *subject){
+	if(subject){
+		if(prerequisites.count(*subject)!=0)
+			prerequisites.erase(*subject);
+	}
+}
+
+void Subject::show_prerequisites(){
+    for(auto prerequisite: prerequisites){
+    	prerequisite.show_subject();
+    }
+}
+
+void Subject::show_subject(){
+	cout << "Name: " << name << endl;
+	cout << "Code: " << code << endl;
+	cout << "Credits: " << credits << endl << endl;
+}
+
+bool Subject::operator<(const Subject &subject) const{
+	return name < subject.name;
 }
--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-    Graph *opa, *other;
-    opa = new Graph("eda", 12345, 4);
-    other = new Graph("c3", 54321, 6);
-    opa->Insert_Edge(other);
-    opa->Show();
-    cout<< opa->code <<endl;
-    cout<< opa->credits <<endl;
+    Subject *opa, *other, *mais_um;
+    opa = new Subject("eda", 12345, 4);
+    other = new Subject("c3", 54321, 6);
+    mais_um = new Subject("c2", 12312, 6);
+    opa->insert_prerequisite(other);
+    opa->insert_prerequisite(mais_um);
+    opa->show_prerequisites();
+    opa->show_subject();
     return 0;
 }
